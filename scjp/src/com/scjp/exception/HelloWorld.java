63d7bd2583af195b84f2a66fd7b4e645b83f356f
Barakathulla <shaikbnisar@gmail.com>
2016-01-16 23:01:57 +0530

    creating Repo on Git Interview Questions

---
+++ scjp/src/com/scjp/exception/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.scjp.exception;
+
+/*
+ * @author Nisar 
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Helloworld first Repo on Git");
+	}
+}
