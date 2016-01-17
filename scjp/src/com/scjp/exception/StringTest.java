3e72f3d03d722dfae414f9be77ce693b7cf3f84a
Barakathulla <shaikbnisar@gmail.com>
2016-01-17 08:12:14 +0530

    changed the logic

--- scjp/src/com/scjp/exception/StringTest.java
+++ scjp/src/com/scjp/exception/StringTest.java
@@ -7,21 +7,24 @@ public class StringTest {
 
 	public static void main(String[] args) {
 		String message = "World Hello";
-		getMessage(message);
+		System.out.println(getMessage(message));
 	}
 
 	/*
 	 * @param String
+	 * 
+	 * return reverse of String word by word
 	 */
-	public static void getMessage(String message) {
+	public static String getMessage(String message) {
 		String msg = "";
 		if (message != null) {
 			String[] data = message.split(" ");
-			for (String str : data) {
-				msg += str;
+			for (int i = data.length - 1; i >= 0; i--) {
+				msg += data[i];
 			}
-			System.out.println(msg);
+
 		}
+		return msg;
 	}
 
 }
